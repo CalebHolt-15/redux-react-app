05e161593ed1fc33fec6d124ab7e7502ea2fedb1
michaelsb-323 <michael.nongphud@sonicbolt.com>
2022-07-20 01:35:18 +0530

    Added dispatch(action) fxn: action: type, payload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux"
 
 function App() {
 
   const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  
+
+  const Increment = () => {
+    dispatch({type: "INCREMENT"})
+  }
+
+  const Decrement = () => {
+    dispatch({type: "DECREMENT"})
+  }
+
+  const AddBy = () => {
+    dispatch({type: "ADDBY", payload: 10})
+  }
+
   return (
     <div>
       <h1>Counter: {counter}</h1>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+      <button onClick={AddBy}>AddBy</button>
    </div>
   );
 }
--- src/store.js/index.js
+++ src/store.js/index.js
@@ -1,6 +1,22 @@
 import { legacy_createStore as createStore } from "redux";
 
-const reducerFn = (state = {counter: 2}, action ) => {
+const reducerFn = (state = {counter: 0}, action ) => {
+    //should be Synchronous
+    //We should not mutate the original state
+    //It should always be a copy of the original state
+
+    if (action.type === "INCREMENT") {
+        return {counter : state.counter + 1}
+    }
+
+    if (action.type === "DECREMENT") {
+        return {counter : state.counter - 1}
+    }
+
+    if (action.type === "ADDBY") {
+        return {counter : state.counter + action.payload}
+    }
+
     return state
 }
 
